5477a7e2e6dfae7fe87a557f0eb7ad775717d774
malik1358 <malik@pinasz.com>
2026-08-05 20:11:55 +0000

    Allow price source URL via environment variable

--- app/lib/priceApiConfig.js
+++ app/lib/priceApiConfig.js
@@ -1,4 +1,7 @@
-export const PRICE_SOURCE_URL =
+const FALLBACK_PRICE_SOURCE_URL =
   "https://script.google.com/macros/s/AKfycbypi2PkOaBIWdcQnxrBJAT6xGmqsuyx2mekiIkkOjE/exec";
+
+export const PRICE_SOURCE_URL =
+  process.env.PRICE_SOURCE_URL || FALLBACK_PRICE_SOURCE_URL;
 
 export const PRICE_CACHE_KEY = "madiba.pricePayload";

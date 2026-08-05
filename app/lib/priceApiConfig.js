66a81f9a5b7fc40a31ae413f878d8617b2c4c009
malik1358 <malik@pinasz.com>
2026-08-05 20:07:06 +0000

    Point price sync to new Google Script endpoint

--- app/lib/priceApiConfig.js
+++ app/lib/priceApiConfig.js
@@ -1,4 +1,4 @@
 export const PRICE_SOURCE_URL =
-  "https://script.google.com/macros/s/AKfycbzXPREoz0tUgern-5LhpEPBMY_ed2hO1fgYpIVfzG2-BU9HbjOklKCBFVMtsw64Uff5/exec";
+  "https://script.google.com/macros/s/AKfycbypi2PkOaBIWdcQnxrBJAT6xGmqsuyx2mekiIkkOjE/exec";
 
 export const PRICE_CACHE_KEY = "madiba.pricePayload";
